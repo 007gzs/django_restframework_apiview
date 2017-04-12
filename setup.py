233cbbbc95e867e56ff4bfb31f931295ddb4487b
007 <007@007.pub>
2017-04-12 18:16:27 +0800

    fix License

--- setup.py
+++ setup.py
@@ -51,16 +51,16 @@ setup(
         'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: LGPL v3 License',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        # 'Programming Language :: Python :: 3', # not test in python 3
+        # 'Programming Language :: Python :: 3.3',
+        # 'Programming Language :: Python :: 3.4',
+        # 'Programming Language :: Python :: 3.5',
     ],
 
     # What does your project relate to?
