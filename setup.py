3758ff3bb02df65f4fd7adbd00b83cb44216ed09
007 <007gzs@gmail.com>
2018-03-08 14:36:16 +0800

    version

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['Django', 'djangorestframework', 'django-import-export'],
+    install_requires=['Django<2', 'djangorestframework', 'django-import-export'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
