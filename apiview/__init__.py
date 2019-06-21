8905ea210c950f3261828936f76350f441b2c7b9
007gzs <007gzs@gmail.com>
2019-06-21 20:24:52 +0800

    Bump version: 1.3.8 → 1.3.9

--- apiview/__init__.py
+++ apiview/__init__.py
@@ -1,7 +1,7 @@
 # encoding: utf-8
 from __future__ import absolute_import, unicode_literals
 
-__version__ = '1.3.8'
+__version__ = '1.3.9'
 __author__ = '007gzs'
 
 default_app_config = 'apiview.apps.ApiViewConfig'
