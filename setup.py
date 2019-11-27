3a6c04a276f1a6453e1b81a6b03644b68e75fc84
007 <007gzs@gmail.com>
2019-11-27 10:47:58 +0800

    Bump version: 1.3.19 → 1.3.20

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.3.19',
+    version='1.3.20',
 
     description='Django REST_framework APIView',
     long_description=long_description,
